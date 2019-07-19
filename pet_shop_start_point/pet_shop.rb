e0e3160d10df586292bb5a5d2fb2a336f8f97e69
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-19 23:59:21 +0100

    minor reworkings to final sell_pet_to_customer code

--- pet_shop_start_point/pet_shop.rb
+++ pet_shop_start_point/pet_shop.rb
@@ -115,6 +115,32 @@ end
 
 #16
 
+def sell_pet_to_customer(pet_shop, pet, customer)
+
+  if pet != nil #so if pet is found
+    find_pet_by_name(pet_shop,pet[:name]) #find the pet
+
+    if customer_can_afford_pet(customer, pet) #check if customer can afford the pet if yes then continue with the methods below
+      remove_customer_cash(customer,pet[:price])
+
+      add_or_remove_cash(pet_shop, pet[:price])
+
+      add_pet_to_customer(customer, pet)
+
+      increase_pets_sold(pet_shop, 1)
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
 #ATTEMPT TO PASS FIRST TEST OF SELL_PET_TO_CUSTOMER
 
 # def sell_pet_to_customer(pet_shop, pet, customer)
@@ -179,56 +205,3 @@ end
 #     end
 #
 # end
-
-
-
-
-
-
-def sell_pet_to_customer(pet_shop, pet, customer)
-
-  if pet == nil #so if pet is not found then end
-
-  else #if pet is not nil so if pet is available
-    find_pet_by_name(pet_shop,pet[:name]) #find the pet
-
-    if customer_can_afford_pet(customer, pet) #check if customer can afford the pet if yes then continue with the methods below
-      remove_customer_cash(customer,pet[:price])
-
-      add_or_remove_cash(pet_shop, pet[:price])
-
-      add_pet_to_customer(customer, pet)
-
-      increase_pets_sold(pet_shop, 1)
-    end
-  end
-end
-
-#     if (find_pet_by_name(pet_shop, pet[:name]) == pet) && customer_can_afford_pet(customer, pet)
-#
-#       remove_customer_cash(customer,pet[:price])
-#
-#       add_or_remove_cash(pet_shop, pet[:price])
-#
-#       add_pet_to_customer(customer, pet)
-#
-#       increase_pets_sold(pet_shop, 1)
-#       #customer_pet_count(customer) instead of 1
-#     end
-#
-#
-# end
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
-
-#
